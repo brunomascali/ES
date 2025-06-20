06c8d28c482bc041cd5e14dae73b202e41fa6d58
Rogerio G. Kannen Dias <rogerkdias@hotmail.com>
2025-06-20 01:39:07 -0300

    corrige nomes de classes

--- backend/src/main/java/com/trabalho/EngSoft/Repository/RideRepository.java
+++ backend/src/main/java/com/trabalho/EngSoft/Repository/RideRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.trabalho.EngSoft.Model.Ride;
 import com.trabalho.EngSoft.Model.User;
+
 
 @Repository
 public interface RideRepository extends JpaRepository<Ride, Long>{
